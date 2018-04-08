bc7c639df981cbd29486d4d6ab9285a14f9465d0
Royzx <592151648@qq.com>
2018-04-08 20:04:37 +0800

    add annoations

---
+++ src/main/java/com/roy/annoations/Recommend.java
@@ -0,0 +1,18 @@
+package com.roy.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @Author: Royzx
+ * @Description: 标记推荐的类或写法
+ * @Date: 2018/4/8.
+ * @Modify by
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface Recommend {
+    String value() default "";
+}
